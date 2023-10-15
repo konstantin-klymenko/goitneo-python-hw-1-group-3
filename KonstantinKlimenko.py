791194125f88fc79b9835f9be6a4ca99d3da74fd
Konstantin <k.m.klymenko@gmail.com>
2023-10-15 14:19:45 +0300

    first commit

---
+++ KonstantinKlimenko.py
@@ -0,0 +1,38 @@
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot created by Konstantin Klimenko!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = user_input.split()
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command.lower() == "hello":
+            print("How can I help you?")
+        elif command.lower() == "add":
+            name, phone = args
+            contacts[name] = phone
+            print("Contact added.")
+        elif command.lower() == "change":
+            name, phone = args
+            if name in contacts:
+                contacts[name] = phone
+                print("Contact updated.")
+            else:
+                print("Contact not found.")
+        elif command.lower() == "phone":
+            name = args[0]
+            if name in contacts:
+                print(contacts[name])
+            else:
+                print("Contact not found.")
+        elif command.lower() == "all":
+            for name, phone in contacts.items():
+                print(f"{name}: {phone}")
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
+
